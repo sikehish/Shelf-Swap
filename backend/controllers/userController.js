8229ad6a43ee45813f74d478566aa95c506cb69f
Syed Hisham Akmal <hisham0502@gmail.com>
2023-08-22 19:39:00 +0530

    Basic backend config modified and incorporated

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -3,6 +3,7 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const User= require('../models/userModel');
 const validator=require('validator');
+const asyncWrapper=require('express-async-handler')
 
 const  path = require('path');
 const { sendMail } = require('../utils/mailFunc');
@@ -11,8 +12,8 @@ const { requestReset, passwordReset } = require('../services/passwords');
 
 
 // Set up routes
-exports.userSignup= async (req, res) => {
-  try {
+exports.userSignup=asyncWrapper(async (req, res) => {
+  // try {
     let { name, email, password, confirmPassword } = req.body;
 
     name=name.trim()
@@ -21,18 +22,31 @@ exports.userSignup= async (req, res) => {
     confirmPassword=confirmPassword.trim()
 
 
-    if (!email || !password || !confirmPassword || !name) throw Error("All fields must be filled");
-    else if (password !== confirmPassword) throw Error("Passwords not matching")
+    if (!email || !password || !confirmPassword || !name) {
+      res.status(400)
+      throw Error("All fields must be filled");
+    }
+    else if (password !== confirmPassword){
+      res.status(400)
+      throw Error("Passwords not matching")
+    }
 
     // Check if the user already exists
     const checkUser = await User.findOne({ email });
     if (checkUser) {
+      res.status(400)
       throw new Error('User already exists');
     }
 
-    if(!validator.isStrongPassword(password)) throw new Error('Password not strong enough!')
+    if(!validator.isStrongPassword(password)) {
+      res.status(400)
+      throw new Error('Password not strong enough!')
+    }
     
-    if(!validator.isEmail(email)) throw new Error("Entered email address not valid!")
+    if(!validator.isEmail(email)) {
+      res.status(400)
+      throw new Error("Entered email address not valid!")
+    }
     
     //Sending an email to the user
     
@@ -55,76 +69,91 @@ exports.userSignup= async (req, res) => {
     res.status(201).json({ status: 'success', data: { name, email} });
 
 
-  } catch (error) {
-    console.error('Error in signup', error);
-    res.status(400).json({status:"fail", message: error.message });
-  }
-};
+  // } catch (error) {
+  //   console.error('Error in signup', error);
+  //   res.status(400).json({status:"fail", message: error.message });
+  // }
+})
 
-exports.userLogin = async (req, res) => {
-  try {
+exports.userLogin =asyncWrapper( async (req, res) => {
+  // try {
     let { email, password } = req.body;
     email=email.trim()
     password=password.trim()
-    if (!email || !password) throw Error("Email and password must be filled");
-        else if (!validator.isEmail(email)) throw Error("Email format invalid");
+    if (!email || !password) {
+      res.status(400)
+      throw Error("Email and password must be filled");
+    }
+        else if (!validator.isEmail(email)) {
+          res.status(400)
+          throw Error("Email format invalid");
+        }
         
         
         // Find the user by email
         const user = await User.findOne({ email });
 
         if (!user) {
+          res.status(404)
           throw new Error("User doesn't exist")
         }
         
         // Compare passwords
         const exists = await bcrypt.compare(password, user.password);
         if (!exists) {
-          throw new Error('Invalid credentials')
+          res.status(401)
+          throw new Error("Incorrect password")
         }
 
-        if(!user.verified) throw new Error("User is not verified")
+        if(!user.verified) {
+          res.status(401)
+          throw new Error("User is not verified")
+        }
 
     // Create a JWT token
     const token = jwt.sign({ id: user._id }, process.env.JWT_KEY, { expiresIn: '5d'});
 
     res.status(200).json({status:"success", data:{ email , name:user.name, token }});
-  } catch (error) {
-    console.error('Error in login', error);
-    res.status(400).json({status:"fail", message: error.message });
-  }
-}
+  // } catch (error) {
+  //   console.error('Error in login', error);
+  //   res.status(400).json({status:"fail", message: error.message });
+  // }
+})
 
 
-exports.userVerify=async (req,res)=>{
+exports.userVerify=asyncWrapper(async (req,res)=>{
   const {token} = req.params;
-  try{
+  // try{
     const { id }= jwt.verify(token,process.env.JWT_KEY)
   console.log(token, id)
   const newDoc = await User.findByIdAndUpdate(id,{ verified: true},{
     new: true
   } )
-  if (!newDoc) throw new Error("User doesn't exist")
+  if (!newDoc) {
+    res.status(404)
+    throw new Error("User doesn't exist")
+  }
   console.log(newDoc)
   // res.status(200).json({status:"success", message:"Verified successfully!"})
   res.sendFile(path.join(__dirname,'..','static','html','verified.html'));
-}catch(err){
-  console.log(err)
-  res.sendFile(path.join(__dirname,'..','static','html','verifiederr.html'));
-  }
+// }catch(err){
+//   console.log(err)
+//   res.sendFile(path.join(__dirname,'..','static','html','verifiederr.html'));
+//   }
   
-}
+})
 
 
-exports.deleteAccount = async (req, res) => {
-  try {
+exports.deleteAccount = asyncWrapper(async (req, res) => {
+  // try {
     const id = req.user;
 
     // Find the user by email
     const user = await User.findByIdAndDelete(id);
 
     if (!user) {
-      return res.status(404).json({ status:"fail",message: 'User not found.' });
+       res.status(404)
+       throw new Error("User doesn't exist")
     }
 
     const url = `${process.env.CLIENT_URL}/signup`
@@ -138,14 +167,14 @@ exports.deleteAccount = async (req, res) => {
     sendMail(user.email,subject, html)
 
     res.status(204).json({ status:"success",message: 'User account deleted successfully.' });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ status:"fail",message: 'An error occurred while deleting the user account.' });
-  }
-}
+  // } catch (error) {
+  //   console.error(error);
+  //   res.status(500).json({ status:"fail",message: 'An error occurred while deleting the user account.' });
+  // }
+})
 
-exports.updateUser= async (req,res)=>{
-  try{
+exports.updateUser= asyncWrapper(async (req,res)=>{
+  // try{
 
   const id = req.user;
   let { name,password,confirmPassword } = req.body;
@@ -154,9 +183,18 @@ exports.updateUser= async (req,res)=>{
   if( password!==undefined && confirmPassword!==undefined){
     password=password.trim()
     confirmPassword=confirmPassword.trim()
-    if(!password || !confirmPassword) throw new Error("Passwords cannot be empty");
-    if (password !== confirmPassword) throw Error("Passwords not matching")
-    if(!validator.isStrongPassword(password)) throw new Error('Password not strong enough!')
+    if(!password || !confirmPassword) {
+      res.status(400)
+      throw new Error("Passwords cannot be empty");
+    }
+    if (password !== confirmPassword) {
+      res.status(400)
+      throw Error("Passwords not matching")
+    }
+    if(!validator.isStrongPassword(password)){
+      res.status(400)
+      throw new Error("Password not strong enough!")
+    }
     
     const newDoc = await User.findByIdAndUpdate(id,{ password },{
       new: true,
@@ -166,51 +204,60 @@ exports.updateUser= async (req,res)=>{
 
   //Updating name
   if(name==undefined){
-    return res.status(404).json({ status:"fail",message: 'No name to update' });
+    res.status(404)  
+    throw new Error("No name to update")
   }
   
   name=name.trim()
 
-  if(!name) throw new Error("Please enter a valid name!")
+  if(!name) {
+    res.status(404)  
+    throw new Error("Please enter a valid name!")
+  }
 
 
   const newDoc = await User.findByIdAndUpdate(id,{ name },{
     new: true,
     runValidators: true
   } )
-  if (!newDoc) throw new Error("User doesn't exist")
+  if (!newDoc) {
+    res.status(404)
+    throw new Error("User doesn't exist")
+  }
   console.log(newDoc)
   res.status(200).json({status:"success", data: newDoc})
-}catch(err){
-  console.log(err)
-  res.status(400).json({status:"fail", message: err.message });
-  }
-}
+// }catch(err){
+//   console.log(err)
+//   res.status(400).json({status:"fail", message: err.message });
+//   }
+})
 
 
-exports.resetRequestController=async (req,res)=>{
-  try{
+exports.resetRequestController=asyncWrapper(async (req,res)=>{
+  // try{
     let { email } = req.body
 
     email=email.trim()
 
-    if(!email) throw new Error("Email cannot be empty");
-    if(!validator.isEmail(email)) throw new Error("Entered email is not valid")
+    if(!email || !validator.isEmail(email)) {
+      res.status(400)
+      throw new Error("Please enter a valid email")
+    }
     
     const data = await requestReset(email)
 
     res.status(200).json({status:"success", data})
 
-  }catch(err){
-    console.log(err)
-    res.status(400).json({status:"fail", message: err.message});
-  }
+  // }catch(err){
+  //   console.log(err)
+  //   res.status(400).json({status:"fail", message: err.message});
+  // }
 
-}
+})
 
-exports.resetPasswordController=async (req,res)=>{
+exports.resetPasswordController=asyncWrapper(async (req,res)=>{
 
-  try{
+  // try{
 
     let { password, confirmPassword, uid, token } = req.body
     
@@ -218,9 +265,9 @@ exports.resetPasswordController=async (req,res)=>{
 
     res.status(200).json({status:"success", data: newDoc})
 
-  }catch(err){
-    console.log(err)
-    res.status(400).json({status:"fail", message: err.message});
-  }
+  // }catch(err){
+  //   console.log(err)
+  //   res.status(400).json({status:"fail", message: err.message});
+  // }
 
-}
+})
